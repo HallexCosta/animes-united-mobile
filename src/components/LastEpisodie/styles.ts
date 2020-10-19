8812c8cffd39cf99694ee8dbda7b056508cf9a79
HallexCosta <hallex.costa@hotmail.com>
2020-10-19 16:12:53 -0300

    Add styles for component LastEpisodie

--- src/components/LastEpisodie/styles.ts
+++ src/components/LastEpisodie/styles.ts
@@ -1,9 +1,30 @@
 import styled from 'styled-components/native'
 
-export const LastEpisodieContainer = styled.View``
+export const LastEpisodieContainer = styled.View`
+  margin-right: 15px;
+`
 
-export const LastEpisodieImage = styled.Image``
+export const LastEpisodieImage = styled.Image`
+  width: 135px;
+  height: 80px;
 
-export const LastEpisodieDescription = styled.Text``
+  opacity: 0.4;
+`
 
-export const LastEpisodieAnimeName = styled.Text``
+export const LastEpisodieDescription = styled.Text`
+  font-family: 'Nunito_400Regular';
+  font-size: 12px;
+  position: absolute;
+  color: #f9f8f7;
+  bottom: 30%;
+  margin-left: 10px;
+`
+
+export const LastEpisodieAnimeName = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  font-size: 12px;
+  position: absolute;
+  color: #f9f8f7;
+  bottom: 10%;
+  margin-left: 10px;
+`

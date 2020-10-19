d579a31674e1399f8a3b59c8ea01d438a34f01bc
HallexCosta <hallex.costa@hotmail.com>
2020-10-19 15:56:47 -0300

    Rename activeButton.ts for index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,32 @@
+import { CategoriesProps, GenresProps, NavigatesProps } from '../pages/Home'
+
+type ActiveButtonHome = CategoriesProps | GenresProps | NavigatesProps
+
+type ActiveButton = ActiveButtonHome
+
+export class Utils {
+  static activeButton<ActiveButton>(
+    buttons: ActiveButton,
+    currentButton: string
+  ): ActiveButton {
+    let newButtons = {} as ActiveButton
+
+    Object.keys(buttons).map(buttonName => {
+      if (buttonName === currentButton) {
+        newButtons = {
+          ...buttons,
+          [currentButton]: true
+        }
+      } else {
+        newButtons = {
+          ...buttons,
+          [Object.keys(buttons)[0]]: false,
+          [buttonName]: false,
+          [currentButton]: true
+        }
+      }
+    })
+
+    return newButtons
+  }
+}
